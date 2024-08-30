d4e58a36b4fb85f08e534ef3736d050ad16fd18e
Chertanov <chertanov@cate.ru>
2024-08-30 14:29:19 +0300

    30.08.2024

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+from statistics import mean
 
 from PIL import Image
 import numpy as np
@@ -10,10 +11,34 @@ k = 1
 r = 1
 
 
-def get_area():
-    for i in range(0, 2 * r + 1):
-        for j in range(0, 2 * r + 1):
-            print(i, j, '\t\t', imageToMatrice[i, j])
+class Point:
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+
+
+def is_black(point_p):
+    mean_p = mean((point_p[0], point_p[1], point_p[2]))
+    if (point_p[0] < 40 and point_p[1] < 40 and point_p[2] < 40 and
+            abs(point_p[0] - mean_p) < 5 and abs(point_p[1] - mean_p) < 5 and abs(point_p[2] - mean_p) < 5):
+        return True
+    else:
+        return False
+
+
+def search_black_point(start_point):
+    for i in range(start_point.y, start_point.y + (2 * r + 1)):
+        for j in range(start_point.x, start_point.x + (2 * r + 1)):
+            if is_black(imageToMatrice[i, j]):
+                return Point(j, i)
+    return
+
+
+def analyze(start_point):
+
+    print('\t\t\t\t\t', imageToMatrice[(start_point.y - 1), start_point.x])
+    print(imageToMatrice[start_point.y, start_point.x - 1], "\t", imageToMatrice[start_point.y, start_point.x], "\t", imageToMatrice[start_point.y, start_point.x + 1])
+    print('\t\t\t\t\t', imageToMatrice[(start_point.y + 1), start_point.x])
     return
 
 
@@ -24,11 +49,12 @@ if __name__ == '__main__':
                    if (isHorizontal := imageToMatrice.shape[1] > imageToMatrice.shape[0])
                    else (imageToMatrice[0] / imageToMatrice[1]))
 
-    get_area()
-
     if isHorizontal:
-        for i in range(r, imageToMatrice.shape[0]):
-            a = 1
+        for i in range(imageToMatrice.shape[0] - 2 * r):
+            if p_b := search_black_point(Point(x=i * k, y=i)):
+                print(imageToMatrice[p_b.y, p_b.x])
+                analyze(p_b)
+                break
     else:
-        for i in range(r, imageToMatrice.shape[1]):
-            a = 1
+        for i in range(imageToMatrice.shape[1] - 2 * r):
+            search_black_point(Point(x=i, y=i*k))
